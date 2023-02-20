888a092cf52f18b6a91ab00f857b885c00f9faaf
Jordanmatutel <donjordanje@gmail.com>
2023-02-19 19:26:18 -0600

    Bugs Fixed

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import time
 import pandas as pd
 import ccxt
-import math
-import matplotlib
+import statistics
+import matplotlib.pyplot as plt
+
+period = 20
+smas = [0] * period
+upperBands = [0] * period
+lowerBands = [0] * period
 
 
 #Stablish the forever-loop to keep the script working
@@ -10,72 +15,60 @@ while True:
 
     pauseTime = 60
 
-    #We use this in case we want to manage the quantity of data we use.
-    def deleteData(Data, period):
-        if Data[period]:
-            del Data[-1]
-        return Data
-
     #We use this in order to calculate the SMA.
     def sma(prices, period):
-        x = sum(prices[-period:]) / period
-        return x
+        if len(prices) < period:
+            return None
+        else:
+            return float(sum(prices[-period:])) / float(period)
 
 
     #We put the market and the broker database from where we want the data
     market = "BTC/USDT"
-
     exchange = ccxt.binance({"Public Key":"XXXXX", "Secret Key":"XXXXX"})
+    prices = exchange.fetch_ohlcv(symbol=market, timeframe="1m", limit=period)
+    closing_prices = [candle[4] for candle in prices]
+    closing = pd.DataFrame(closing_prices)
+    closing = closing.astype(float)
 
     #We input the last 20 prices using the data from the CCXT exchange. In this case
     #We are using the prices from Binance. We also input the period we want to work with.
 
-    #currentPrice = exchange.fetch_ohlcv(market, timeframe='1m')
-    period = 20
-    prices = exchange.fetch_trades(symbol='BTC/USDT', limit=period)
     date = [None] * period
-
-    for i in range(1, period):
+    for i in range(0, period):
         date[i] = i
 
-
+    dates = pd.DataFrame(date)
+    dates = dates.astype(int)
     # We stablish the current SMA using the period and we add to the database
-    smas = []
-    smas.__add__(sma(prices, period))
 
-    deleteData(prices,period)
-    deleteData(smas, period)
+    c = sma(closing_prices, period)
+    smas[1:] = smas[:-1]
+    smas[0] = c
 
     #Now that we got the current price and the SMA, we start calculating the upper band and the lower band
-    stdev = math.sqrt(prices)
-    upperBand = sma(prices, period) + (2 * stdev)
-    lowerBand = sma(prices, period) - (2 * stdev)
+    stdev = statistics.stdev(closing_prices)
+    upperBand = sma(closing_prices, period) + (2 * stdev)
+    lowerBand = sma(closing_prices, period) - (2 * stdev)
 
-    upperBands = []
-    upperBands.__add__(upperBand)
-    deleteData(upperBands, period)
+    upperBands[1:] = upperBands[:-1]
+    upperBands[0] = upperBand
 
-    lowerBands = []
-    lowerBands.__add__(lowerBand)
-    deleteData(lowerBands, period)
-
-
-    result = {"Dates":[date], "Prices":[prices],"SMA":[smas],
-              "Upper Band":[upperBands], "Lower Band":[lowerBands]}
-
-    df = pd.Dataframe (result)
+    lowerBands[1:] = lowerBands[:-1]
+    lowerBands[0] = lowerBand
 
     #Now that we got the dataframes for the prices, the SMA and the bands we can now
     #plot the graph showing the bands of bollinger
 
-    chart = df.plot(x = "Dates", y = "Prices", label = f"Last {period} Prices")
-    df.plot(x = "Dates", y = "SMA", label = f"The {period} SMA", ax = chart)
-    df.plot(x = "Dates", y = "Upper Bands", label = f"The {period} Upper Band", ax = chart)
-    df.plot(x = "Dates", y = "Lower Bands", label = f"The {period} Lower Band", ax = chart)
+    chart = plt.plot(date, closing)
+    plt.plot(date, smas)
+    plt.plot(date, upperBands)
+    plt.plot(date, lowerBands)
+    plt.xlabel('Dates')
+    plt.ylabel('Prices')
+    plt.title('Bollinger Bands using Python')
+    plt.show()
 
-    df.xlabel('Dates')
-    df.ylabel('Prices')
-    df.title('Bollinger Bands using Python')
-    df.show()
-
-    time.sleep(pauseTime)
+    print(smas)
+    print(smas.__len__())
+    time.sleep(1)
